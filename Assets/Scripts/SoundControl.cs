2c67bf15e7d23191d8ce1f5db4eb2e3ab10b942f
jack <jack.enqvist@gmail.com>
2014-10-11 01:25:32 +0200

    Added music and mute button, moved some scripts and added a level

---
+++ Assets/Scripts/SoundControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundControl : MonoBehaviour {
+
+	AudioSource audioSource;
+	bool mute = false;
+
+	float fadeOutTime = 2f;
+
+	void Start () {
+		audioSource = GameObject.Find("Music").GetComponent<AudioSource>();	
+	}
+	
+	void Update () {
+		float targetVolume = mute ? 0f : 1f;
+		audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, Time.deltaTime * fadeOutTime);
+	}
+
+	public void ToggleMute(){
+		mute = !mute;
+
+	}
+}
