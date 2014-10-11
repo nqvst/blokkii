f91b1899cf579e19999097f1f016aa1ecd2f2a67
jack <jack.enqvist@gmail.com>
2014-10-11 13:03:44 +0200

    Fixed muting music with play and pause

--- Assets/Scripts/SoundControl.cs
+++ Assets/Scripts/SoundControl.cs
@@ -1,24 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SoundControl : MonoBehaviour {
+public class SoundControl : MonoBehaviour
+{
 
 	AudioSource audioSource;
 	bool mute = false;
-
 	float fadeOutTime = 2f;
 
-	void Start () {
-		audioSource = GameObject.Find("Music").GetComponent<AudioSource>();	
-	}
-	
-	void Update () {
-		float targetVolume = mute ? 0f : 1f;
-		audioSource.volume = Mathf.Lerp(audioSource.volume, targetVolume, Time.deltaTime * fadeOutTime);
+	void Start ()
+	{
+		audioSource = GameObject.Find ("Music").GetComponent<AudioSource> ();	
 	}
 
-	public void ToggleMute(){
+	void Update ()
+	{
+		float targetVolume = mute ? 0f : 1f;
+		if (audioSource.volume != targetVolume) {
+			audioSource.volume = Mathf.Lerp (audioSource.volume, targetVolume, Time.deltaTime * fadeOutTime);
+		}
+
+		if(Mathf.Abs(targetVolume - audioSource.volume) < 0.1 && targetVolume == 0) {
+			StopMusic();
+		}
+		Debug.Log(targetVolume);
+
+
+	}
+
+	void StopMusic ()
+	{
+		audioSource.Pause ();
+	}
+
+	void ResumeMusic ()
+	{
+		audioSource.Play ();
+	}
+
+	public void ToggleMute ()
+	{
 		mute = !mute;
+		Debug.Log(mute);
+		if(!mute){
+			ResumeMusic();
+		}
 
 	}
 }
